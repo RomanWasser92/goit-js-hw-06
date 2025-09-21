3e9d51557ca1d830a98e53ceec7da8a9e50326c5
Roman Waßer <roman_wasser@web.de>
2025-09-21 17:28:36 +0200

    DZ-JS-06

--- js/task-3.js
+++ js/task-3.js
@@ -1,28 +1,28 @@
-
-
-const profile = {
-
-    userName: 'Jacob',
-    playTime: 300,
-
-    changeUserName(newName) {
-        this.userName = newName;
-    },
-
-    updatePlayTime(hours) {
-        this.playTime += hours;
-    },
-
-    getInfo() {
-        return `${this.userName} has ${this.playTime} active hours!`;
+class StringBuilder{
+    #value;
+    constructor(initialValue){
+        this.#value = initialValue;
     }
+    getValue(){
+        return this.#value;
+    }
+    padEnd(str){
+        this.#value += str;
+    }
+    padStart(str) {
+    this.#value = str + this.#value;
+  }
+  padBoth(str) {
+    this.#value = str + this.#value + str;
+  }
 }
 
 
-console.log(profile.getInfo()); // "Jacob has 300 active hours!"
-
-profile.changeUserName("Marco");
-console.log(profile.getInfo()); // "Marco has 300 active hours!"
-
-profile.updatePlayTime(20);
-console.log(profile.getInfo()); // "Marco has 320 active hours!"
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
